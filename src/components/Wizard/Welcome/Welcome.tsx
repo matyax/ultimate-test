9a87e1f952d9d2fd6acba7e245b138ddf965262c
Matias Chomicki <matyax@gmail.com>
2022-07-07 19:00:10 +0200

    Add robot to Wizard welcome

--- src/components/Wizard/Welcome/Welcome.tsx
+++ src/components/Wizard/Welcome/Welcome.tsx
@@ -1,5 +1,27 @@
+import RobotImage from "resources/robot.png";
+
 const Welcome = () => {
-  return <div>Welcome to the Wizard</div>;
+  return (
+    <div>
+      <h1 className="wizard__title">Customize your Robot</h1>
+      <p>Welcome to the Robot Customization Wizard.</p>
+      <div className="wizard__robot-container">
+        <img src={RobotImage} alt="Robot" className="wizard__robot" />
+      </div>
+      <p>
+        Complete the following steps to configure different aspects of your
+        agent, to provide your customers with the best possible experience.{" "}
+        <a
+          href="https://support.ultimate.ai/hc/en-us/articles/4405740079506-What-is-an-intent-"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn more
+        </a>
+        .
+      </p>
+    </div>
+  );
 };
 
 export default Welcome;
