0dab4e9653ad76e95aa7c53b1d67d2a7140fd80f
Matias Chomicki <matyax@gmail.com>
2022-07-07 18:59:56 +0200

    And next and prev buttons

--- src/components/Wizard/Wizard.tsx
+++ src/components/Wizard/Wizard.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Button } from "react-bootstrap";
 import Steps from "components/Steps";
 import Welcome from "components/Wizard/Welcome";
 import "./Wizard.css";
@@ -6,12 +7,30 @@ import "./Wizard.css";
 const Wizard = () => {
   const [step, setStep] = useState(1);
 
+  const prevStep = () => {
+    setStep(step - 1);
+  };
+
+  const nextStep = () => {
+    setStep(step + 1);
+  };
+
   return (
     <div className="wizard">
       <div className="wizard__header">
         <Steps current={step} total={4} />
       </div>
       <div className="wizard__body">{step === 1 && <Welcome />}</div>
+      <div className="wizard__footer">
+        <Button variant="primary" onClick={nextStep}>
+          Next
+        </Button>
+        {step > 1 && (
+          <Button variant="secondary" onClick={prevStep}>
+            Back
+          </Button>
+        )}
+      </div>
     </div>
   );
 };
